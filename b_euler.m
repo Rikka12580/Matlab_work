4d1bdb359e1f1a634e702a10f60be3b44dc57dc4
Lacey Conrad <laco9372@colorado.edu>
2017-06-26 13:08:12 -0600

    Update and rename beuler.m to b_euler.m

--- b_euler.m
+++ b_euler.m
@@ -8,10 +8,7 @@ y(1,:) = ya;
 t(1) = a;
 for i = 1 : n
     tn = tn + h;
-    for k = 1:5 % Few iterations of Newton's method, for simplicity
-        % In a more sophisticated implementation, Newton's method should 
-        % be stopped depending on how its error compares with an estimated 
-        % local truncation error
+    for k = 1:5 
         x = x - (h * df(tn,x) - 1) \ (h * f(tn,x) - x + wi);
     end;
     wi = x;
